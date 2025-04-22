fcb3848c9d26cbdd29ead511d928f5e365b83cf5
ReadyToDevNow <learntolearn0@gmail.com>
2025-04-22 11:34:40 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <nav></nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>

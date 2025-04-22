06642284914500e1e2c6673f3e3b0651f0ca1dea
ReadyToDevNow <learntolearn0@gmail.com>
2025-04-22 16:35:09 +0200

    feat(design): add red color pack

--- src/components/ProductSheet.vue
+++ src/components/ProductSheet.vue
@@ -20,23 +20,29 @@
 -->
     <!-- Main content - 1 card per row -->
     <main class="p-6 space-y-6 max-w-3xl mx-auto">
-      <div class="flex flex-row p-4 items-center text-white">
+      <h1 class="bg-red-500 p-12">Palette de couleur</h1>
+      <div class="flex flex-row p-4 items-center text-white w-full">
         <div class="flex flex-col mr-4">
-          <span class="bg-[#F44336]">Red</span>
-          <span class="bg-[#FFEBEE] text-black">red-lighten-5</span>
-          <span class="bg-[#FFCDD2] text-black">red-lighten-4</span>
-          <span class="bg-[#EF9A9A] text-black">red-lighten-3</span>
-          <span class="bg-[#E57373]">red-lighten-2</span>
-          <span class="bg-[#EF5350]">red-lighten-1</span>
-          <span class="bg-[#E53935]">red-darken-1</span>
-          <span class="bg-[#D32F2F]">red-darken-2</span>
-          <span class="bg-[#C62828]">red-darken-3</span>
-          <span class="bg-[#B71C1C]">red-darken-4</span>
-          <span class="bg-[#FF8A80]">red-accent-1</span>
-          <span class="bg-[#FF5252]">red-accent-2</span>
-          <span class="bg-[#FF1744]">red-accent-3</span>
-          <span class="bg-[#D50000]">red-accent-4</span>
-          <span>GodPack</span>
+          <span class="bg-[#F44336] p-4">Red</span>
+          <span class="bg-[#FFEBEE] text-black p-2"
+            >red-lighten-5 <span class="m-10"></span>#FFEBEE</span
+          >
+          <span class="bg-[#FFCDD2] text-black p-2"
+            >red-lighten-4 <span class="m-10"></span>#FFCDD2</span
+          >
+          <span class="bg-[#EF9A9A] text-black p-2"
+            >red-lighten-3<span class="m-10"> </span>#EF9A9A</span
+          >
+          <span class="bg-[#E57373] p-2">red-lighten-2<span class="m-10"></span>#E57373</span>
+          <span class="bg-[#EF5350] p-2">red-lighten-1 <span class="m-10"></span>#EF5350</span>
+          <span class="bg-[#E53935] p-2">red-darken-1<span class="m-10"></span>#FF8A80</span>
+          <span class="bg-[#D32F2F] p-2">red-darken-2 <span class="m-10"></span>#D32F2F</span>
+          <span class="bg-[#C62828] p-2">red-darken-3 <span class="m-10"></span>#C62828</span>
+          <span class="bg-[#B71C1C] p-2">red-darken-4<span class="m-10"></span>#B71C1C</span>
+          <span class="bg-[#FF8A80] p-2">red-accent-1<span class="m-10"></span>#FF8A80</span>
+          <span class="bg-[#FF5252] p-2">red-accent-2 <span class="m-10"></span>#FF5252</span>
+          <span class="bg-[#FF1744] p-2">red-accent-3<span class="m-10"></span>#FF1744</span>
+          <span class="bg-[#D50000] p-2">red-accent-4<span class="m-10"></span>#D50000</span>
         </div>
         <!--
         <div class="flex flex-col mr-4">

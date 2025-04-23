cd3429e506c62d6e4489102dc5d40531527f95c2
ReadyToDevNow <learntolearn0@gmail.com>
2025-04-23 18:05:49 +0200

    feat(): Pink color palette

--- src/components/ProductSheet.vue
+++ src/components/ProductSheet.vue
@@ -19,30 +19,154 @@
     </section>
 -->
     <!-- Main content - 1 card per row -->
-    <main class="p-6 space-y-6 max-w-3xl mx-auto">
+    <main>
       <h2 class="bg-red-900 p-12 text-white">Palettes de couleur</h2>
-      <div class="flex flex-row p-4 items-center text-white w-full">
-        <div class="flex flex-col mr-4">
-          <span class="bg-[#F44336] p-4">Red</span>
-          <span class="bg-[#FFEBEE] text-black p-2"
-            >red-lighten-5 <span class="m-10"></span>#FFEBEE</span
-          >
-          <span class="bg-[#FFCDD2] text-black p-2"
-            >red-lighten-4 <span class="m-10"></span>#FFCDD2</span
-          >
-          <span class="bg-[#EF9A9A] text-black p-2"
-            >red-lighten-3<span class="m-10"> </span>#EF9A9A</span
-          >
-          <span class="bg-[#E57373] p-2">red-lighten-2<span class="m-10"></span>#E57373</span>
-          <span class="bg-[#EF5350] p-2">red-lighten-1 <span class="m-10"></span>#EF5350</span>
-          <span class="bg-[#E53935] p-2">red-darken-1<span class="m-10"></span>#FF8A80</span>
-          <span class="bg-[#D32F2F] p-2">red-darken-2 <span class="m-10"></span>#D32F2F</span>
-          <span class="bg-[#C62828] p-2">red-darken-3 <span class="m-10"></span>#C62828</span>
-          <span class="bg-[#B71C1C] p-2">red-darken-4<span class="m-10"></span>#B71C1C</span>
-          <span class="bg-[#FF8A80] p-2">red-accent-1<span class="m-10"></span>#FF8A80</span>
-          <span class="bg-[#FF5252] p-2">red-accent-2 <span class="m-10"></span>#FF5252</span>
-          <span class="bg-[#FF1744] p-2">red-accent-3<span class="m-10"></span>#FF1744</span>
-          <span class="bg-[#D50000] p-2">red-accent-4<span class="m-10"></span>#D50000</span>
+      <div class="p-6 space-y-6">
+        <div class="flex flex-row p-4 items-center text-white w-full">
+          <div class="flex flex-col mr-4">
+            <span class="bg-[#F44336] p-4">Red</span>
+            <div class="flex flex-col">
+              <span class="bg-[#FFEBEE] text-black p-2"
+                >red-lighten-5 <span class="m-10"></span>#FFEBEE</span
+              >
+              <span class="bg-[#FFCDD2] text-black p-2"
+                >red-lighten-4 <span class="m-10"></span>#FFCDD2</span
+              >
+              <span class="bg-[#EF9A9A] text-black p-2"
+                >red-lighten-3<span class="m-10"> </span>#EF9A9A</span
+              >
+              <span class="bg-[#E57373] p-2">red-lighten-2<span class="m-10"></span>#E57373</span>
+              <span class="bg-[#EF5350] p-2">red-lighten-1 <span class="m-10"></span>#EF5350</span>
+              <span class="bg-[#E53935] p-2">red-darken-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#D32F2F] p-2">red-darken-2 <span class="m-10"></span>#D32F2F</span>
+              <span class="bg-[#C62828] p-2">red-darken-3 <span class="m-10"></span>#C62828</span>
+              <span class="bg-[#B71C1C] p-2">red-darken-4<span class="m-10"></span>#B71C1C</span>
+              <span class="bg-[#FF8A80] p-2">red-accent-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#FF5252] p-2">red-accent-2 <span class="m-10"></span>#FF5252</span>
+              <span class="bg-[#FF1744] p-2">red-accent-3<span class="m-10"></span>#FF1744</span>
+              <span class="bg-[#D50000] p-2">red-accent-4<span class="m-10"></span>#D50000</span>
+            </div>
+          </div>
+          <div class="flex flex-col mr-4">
+            <span class="bg-[#E91E63] p-4">pink</span>
+            <div class="flex flex-col">
+              <span class="bg-[#FCE4EC] text-black p-2"
+                >pink-lighten-5 <span class="m-10"></span>#FFEBEE</span
+              >
+              <span class="bg-[#F8BBD0] text-black p-2"
+                >pink-lighten-4 <span class="m-10"></span>#FFCDD2</span
+              >
+              <span class="bg-[#F48FB1] text-black p-2"
+                >pink-lighten-3<span class="m-10"> </span>#EF9A9A</span
+              >
+              <span class="bg-[#F06292] p-2">pink-lighten-2<span class="m-10"></span>#F06292</span>
+              <span class="bg-[#EC407A] p-2">pink-lighten-1 <span class="m-10"></span>#EC407A</span>
+              <span class="bg-[#D81B60] p-2">pink-darken-1<span class="m-10"></span>#D81B60</span>
+              <span class="bg-[#C2185B] p-2">pink-darken-2 <span class="m-10"></span>#C2185B</span>
+              <span class="bg-[#AD1457] p-2">pink-darken-3 <span class="m-10"></span>#AD1457</span>
+              <span class="bg-[#880E4F] p-2">pink-darken-4<span class="m-10"></span>#880E4F</span>
+              <span class="bg-[#FF80AB] p-2">pink-accent-1<span class="m-10"></span>#FF80AB</span>
+              <span class="bg-[#FF4081] p-2">pink-accent-2 <span class="m-10"></span>#FF4081</span>
+              <span class="bg-[#F50057] p-2">pink-accent-3<span class="m-10"></span>#F50057</span>
+              <span class="bg-[#C51162] p-2">pink-accent-4<span class="m-10"></span>#C51162</span>
+            </div>
+          </div>
+          <div class="flex flex-col mr-4">
+            <span class="bg-[#F44336] p-4">Red</span>
+            <div class="flex flex-col">
+              <span class="bg-[#FFEBEE] text-black p-2"
+                >red-lighten-5 <span class="m-10"></span>#FFEBEE</span
+              >
+              <span class="bg-[#FFCDD2] text-black p-2"
+                >red-lighten-4 <span class="m-10"></span>#FFCDD2</span
+              >
+              <span class="bg-[#EF9A9A] text-black p-2"
+                >red-lighten-3<span class="m-10"> </span>#EF9A9A</span
+              >
+              <span class="bg-[#E57373] p-2">red-lighten-2<span class="m-10"></span>#E57373</span>
+              <span class="bg-[#EF5350] p-2">red-lighten-1 <span class="m-10"></span>#EF5350</span>
+              <span class="bg-[#E53935] p-2">red-darken-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#D32F2F] p-2">red-darken-2 <span class="m-10"></span>#D32F2F</span>
+              <span class="bg-[#C62828] p-2">red-darken-3 <span class="m-10"></span>#C62828</span>
+              <span class="bg-[#B71C1C] p-2">red-darken-4<span class="m-10"></span>#B71C1C</span>
+              <span class="bg-[#FF8A80] p-2">red-accent-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#FF5252] p-2">red-accent-2 <span class="m-10"></span>#FF5252</span>
+              <span class="bg-[#FF1744] p-2">red-accent-3<span class="m-10"></span>#FF1744</span>
+              <span class="bg-[#D50000] p-2">red-accent-4<span class="m-10"></span>#D50000</span>
+            </div>
+          </div>
+          <div class="flex flex-col mr-4">
+            <span class="bg-[#F44336] p-4">Red</span>
+            <div class="flex flex-col">
+              <span class="bg-[#FFEBEE] text-black p-2"
+                >red-lighten-5 <span class="m-10"></span>#FFEBEE</span
+              >
+              <span class="bg-[#FFCDD2] text-black p-2"
+                >red-lighten-4 <span class="m-10"></span>#FFCDD2</span
+              >
+              <span class="bg-[#EF9A9A] text-black p-2"
+                >red-lighten-3<span class="m-10"> </span>#EF9A9A</span
+              >
+              <span class="bg-[#E57373] p-2">red-lighten-2<span class="m-10"></span>#E57373</span>
+              <span class="bg-[#EF5350] p-2">red-lighten-1 <span class="m-10"></span>#EF5350</span>
+              <span class="bg-[#E53935] p-2">red-darken-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#D32F2F] p-2">red-darken-2 <span class="m-10"></span>#D32F2F</span>
+              <span class="bg-[#C62828] p-2">red-darken-3 <span class="m-10"></span>#C62828</span>
+              <span class="bg-[#B71C1C] p-2">red-darken-4<span class="m-10"></span>#B71C1C</span>
+              <span class="bg-[#FF8A80] p-2">red-accent-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#FF5252] p-2">red-accent-2 <span class="m-10"></span>#FF5252</span>
+              <span class="bg-[#FF1744] p-2">red-accent-3<span class="m-10"></span>#FF1744</span>
+              <span class="bg-[#D50000] p-2">red-accent-4<span class="m-10"></span>#D50000</span>
+            </div>
+          </div>
+          <div class="flex flex-col mr-4">
+            <span class="bg-[#F44336] p-4">Red</span>
+            <div class="flex flex-col">
+              <span class="bg-[#FFEBEE] text-black p-2"
+                >red-lighten-5 <span class="m-10"></span>#FFEBEE</span
+              >
+              <span class="bg-[#FFCDD2] text-black p-2"
+                >red-lighten-4 <span class="m-10"></span>#FFCDD2</span
+              >
+              <span class="bg-[#EF9A9A] text-black p-2"
+                >red-lighten-3<span class="m-10"> </span>#EF9A9A</span
+              >
+              <span class="bg-[#E57373] p-2">red-lighten-2<span class="m-10"></span>#E57373</span>
+              <span class="bg-[#EF5350] p-2">red-lighten-1 <span class="m-10"></span>#EF5350</span>
+              <span class="bg-[#E53935] p-2">red-darken-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#D32F2F] p-2">red-darken-2 <span class="m-10"></span>#D32F2F</span>
+              <span class="bg-[#C62828] p-2">red-darken-3 <span class="m-10"></span>#C62828</span>
+              <span class="bg-[#B71C1C] p-2">red-darken-4<span class="m-10"></span>#B71C1C</span>
+              <span class="bg-[#FF8A80] p-2">red-accent-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#FF5252] p-2">red-accent-2 <span class="m-10"></span>#FF5252</span>
+              <span class="bg-[#FF1744] p-2">red-accent-3<span class="m-10"></span>#FF1744</span>
+              <span class="bg-[#D50000] p-2">red-accent-4<span class="m-10"></span>#D50000</span>
+            </div>
+          </div>
+          <div class="flex flex-col mr-4">
+            <span class="bg-[#F44336] p-4">Red</span>
+            <div class="flex flex-col">
+              <span class="bg-[#FFEBEE] text-black p-2"
+                >red-lighten-5 <span class="m-10"></span>#FFEBEE</span
+              >
+              <span class="bg-[#FFCDD2] text-black p-2"
+                >red-lighten-4 <span class="m-10"></span>#FFCDD2</span
+              >
+              <span class="bg-[#EF9A9A] text-black p-2"
+                >red-lighten-3<span class="m-10"> </span>#EF9A9A</span
+              >
+              <span class="bg-[#E57373] p-2">red-lighten-2<span class="m-10"></span>#E57373</span>
+              <span class="bg-[#EF5350] p-2">red-lighten-1 <span class="m-10"></span>#EF5350</span>
+              <span class="bg-[#E53935] p-2">red-darken-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#D32F2F] p-2">red-darken-2 <span class="m-10"></span>#D32F2F</span>
+              <span class="bg-[#C62828] p-2">red-darken-3 <span class="m-10"></span>#C62828</span>
+              <span class="bg-[#B71C1C] p-2">red-darken-4<span class="m-10"></span>#B71C1C</span>
+              <span class="bg-[#FF8A80] p-2">red-accent-1<span class="m-10"></span>#FF8A80</span>
+              <span class="bg-[#FF5252] p-2">red-accent-2 <span class="m-10"></span>#FF5252</span>
+              <span class="bg-[#FF1744] p-2">red-accent-3<span class="m-10"></span>#FF1744</span>
+              <span class="bg-[#D50000] p-2">red-accent-4<span class="m-10"></span>#D50000</span>
+            </div>
+          </div>
         </div>
 
         <!--
